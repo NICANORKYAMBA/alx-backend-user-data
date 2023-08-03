fe7c4ff06587d7b856f9d8fa35fede414b12f38b
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-03 13:04:32 +0300

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,7 @@ Created on Thur Aug  03 12:00:00 2023
 @Author: Nicanor Kyamba
 """
 import re
+import logging
 from typing import List
 
 
@@ -33,3 +34,48 @@ def filter_datum(fields: List[str], redaction: str,
     pattern = '|'.join(fields)
     return re.sub(
             rf'({pattern})=([^{separator}]*)', rf'\1={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        """
+        Constructor
+
+        Parameters
+        ----------
+        fields : list
+            List of fields to filter
+
+        Returns
+        -------
+        None
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats a record
+
+        Parameters
+        ----------
+        record : logging.LogRecord
+            Record to format
+
+        Returns
+        -------
+        str
+            Formatted record
+        """
+        log_message = super().format(record)
+        for field in self.fields:
+            pattern = rf'(?<={field}=)[^{self.SEPARATOR}\
+                    ]*(?={self.SEPARATOR})|(?<={field}=)[^{self.SEPARATOR}]*$'
+            log_message = re.sub(pattern, self.REDACTION, log_message)
+        return log_message

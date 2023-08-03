0b3edf5ac979c971366d26ae302d702191d6190d
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-03 13:27:50 +0300

    Fixed code requirement for less than 5 lines

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -44,7 +44,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
+    def __init__(self, fields: List[str]):
         """
         Constructor
 
@@ -57,7 +57,7 @@ class RedactingFormatter(logging.Formatter):
         -------
         None
         """
-        super().__init__(self.FORMAT)
+        super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
@@ -74,6 +74,6 @@ class RedactingFormatter(logging.Formatter):
         str
             Formatted record
         """
-        record.msg = filter_datum(
-            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
-        return super().format(record)
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)

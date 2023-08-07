3491a452fcc958c2bbe843b19d536f97dd288295
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-07 14:54:28 +0300

    6. Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,11 +16,14 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 auth = None
-auth_type = getenv("AUTH_TYPE")
+AUTH_TYPE = getenv("AUTH_TYPE")
 
-if auth_type == "auth":
+if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.before_request
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  07 15:00:00 2023
+
+@Author: Nicanor Kyamba
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Basic Authentication class
+    """
+    pass

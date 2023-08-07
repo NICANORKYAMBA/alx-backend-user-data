51483c38112183ce596611be59e9e0aa216a261e
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-07 13:55:22 +0300

    4. Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -29,7 +29,22 @@ class Auth:
         bool
             True if path is allowed
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        normalized_path = path if path.endswith('/') else path + '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/'):
+                if normalized_path == excluded_path:
+                    return False
+            elif normalized_path.startswith(excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """

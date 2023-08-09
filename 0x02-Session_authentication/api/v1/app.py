3aaf4813d3d204d4ba833843db38c855afacca0c
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-09 21:49:00 +0300

    0. Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,13 +43,12 @@ def before_request():
         "/api/v1/forbidden/",
         "/api/v1/auth_session/login/",
     ]
+
     if not auth.require_auth(request.path, allowed_paths):
         return
 
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
 
     current_user = auth.current_user(request)

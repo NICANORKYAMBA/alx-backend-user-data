d1b1bad8810d1445f4c99b72aec30cc6df89f1b5
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-09 14:58:58 +0300

    2. Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,9 +5,35 @@ Created on Wed Aug  09 14:00:00 2023
 
 @Author: Nicanor Kyamba
 """
+import uuid
+from typing import Dict
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Class SessionAuth"""
-    pass
+
+    user_id_by_session_id: Dict[str, str] = {}
+
+    def create_session(
+            self, user_id: str = None) -> str:
+        """
+        Method to create a session
+
+        Args:
+            user_id (str, optional): user id. Defaults to None.
+
+        Returns:
+            str: session id
+        """
+        if user_id is None:
+            return None
+
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id

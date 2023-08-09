95457c6bd406f32f7c916897d7e7a048d98a2223
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-09 14:45:54 +0300

    1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  09 14:00:00 2023
+
+@Author: Nicanor Kyamba
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class SessionAuth"""
+    pass

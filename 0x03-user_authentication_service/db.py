ed4e9b767d5e5a84be3f7e12a4d645c6dfecc5f6
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-14 14:41:56 +0300

    Query table in User database

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -11,7 +11,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import IntegrityError, InvalidRequestError
 
 from user import Base
 from user import User
@@ -56,3 +57,14 @@ class DB:
         except IntegrityError:
             self._session.rollback()
             return ValueError("User already exists")
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user in the database
+        """
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound as e:
+            raise e
+        except InvalidRequestError as error:
+            raise error

d78bdc860809fba768f0d5ebe858b92c3c929940
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-14 13:24:45 +0300

    Implemented a user model with sqlalchemy

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  14 13:00:00 2023
+
+@Author: Nicanor Kyamba
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    User model
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)

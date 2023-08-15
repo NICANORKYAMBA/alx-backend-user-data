b903263be1904047305758c37456c1c464c67f20
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-15 12:04:51 +0300

    Register a user using the app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,10 +5,15 @@ Created on Tue Aug  15 09:00:00 2023
 
 @Author: Nicanor Kyamba
 """
-from flask import Flask, jsonify
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
+from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
+
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -17,6 +22,23 @@ def index():
     Returns the index page
     """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """
+    Route to register a new user
+    """
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = AUTH.register_user(email, password)
+
+        return jsonify({"email": "{}".format(user.email),
+                        "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':

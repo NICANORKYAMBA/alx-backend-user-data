b027258c1d22839886fcbeb8954a0ce76988ae58
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-08-15 18:22:08 +0300

    Fixed code requirement

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -121,7 +121,7 @@ def get_reset_password_token():
     try:
         reset_token = AUTH.get_reset_password_token(email)
         return jsonify({"email": email, "reset_token": reset_token}), 200
-    except NoResultFound:
+    except Exception:
         abort(403)
 
 
